7ebffa3ef65c076bc9d51c95648a04c0fd81bf10
bsc-inf-09-20 <bsc-inf-09-20@unima.ac.mw>
2023-04-13 12:25:24 +0200

    update

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.get('/api/v1/posts', function (req, res) {
 app.listen(3000, function () {
     console.log('dinedrop listening on port 3000')
 
-    dbConnection.connect(function (err) {
-        if (err) throw err
-
         console.log("Connected to MySQL")
     })
 })
