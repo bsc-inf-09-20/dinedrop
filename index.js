aec3c46bfd5c5e4a722e2c7f9b07ec9372dc46c9
bsc-inf-09-20 <bsc-inf-09-20@unima.ac.mw>
2023-04-13 12:24:22 +0200

    update

--- index.js
+++ index.js
@@ -4,30 +4,11 @@ const app = express()
 
 
 app.get('/api/v1/posts', function (req, res) {
-    var sql2 = "SElECT * FROM posts"
-    return dbConnection.query(sql2, function (err, result) {
-        if (err) throw err;
 
         return res.json(result)
     })
-})
 
-// Create new post/article in the database
-app.post('/api/v1/posts', function (req, res) {
-    // console.log(req.body)
-    const { name, imageUrl, summary } = req.body // destructure sent properties from the REQUEST body
-
-    // construct sql query
-    var sql = `INSERT INTO posts (name, imageUrl, summary) VALUES ('${name}','${imageUrl}','${summary}')`;
-    // console.log(sql)
-    // Query the MySQL database and return result to the client app ie. POSTMAN or Web APP
-    return dbConnection.query(sql, function (err, result) {
-        if (err) throw err; // if error throw it, else continue execution to next line
-        return res.json(result)
-        // console.log("1 record inserted");
-    });
-    // res.json(sql)
-})
+/
 
 app.listen(3000, function () {
     console.log('dinedrop listening on port 3000')
